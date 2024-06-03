b93d826eb69a3ee3a5dd3b5dbb23fa8608276b45
richieArnold <arnoldrichie31.rar@gmail.com>
2024-06-03 15:05:28 +0530

    add signup page

---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "../styles/SignUp.css";
+function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contact, setContact] = useState("");
+
+  let handleSubmit = (event) => {
+    let inputObj = { name, email, password, contact };
+    console.log(inputObj);
+    event.preventDefault();
+  };
+  return (
+    <form className="myForm">
+      <label for="name" id="label">
+        Name
+      </label>
+      <input
+        type="text"
+        id="name input"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <label for="email" id="label">
+        Email
+      </label>
+      <input
+        type="email"
+        id="email input"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <label for="password" id="label">
+        Password
+      </label>
+      <input
+        type="password"
+        id="password input"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <label for="contact" id="label">
+        Contact
+      </label>
+      <input
+        type="text"
+        id="contact input"
+        onChange={(e) => {
+          setContact(e.target.value);
+        }}
+      />
+      <button type="submit" id="input" onClick={handleSubmit}>
+        Sign Up
+      </button>
+    </form>
+  );
+}
+
+export default SignUp;
