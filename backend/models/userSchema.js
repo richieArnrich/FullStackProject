82f4ca64a42c5420124f482acaf7d1a652f64b97
richieArnold <arnoldrichie31.rar@gmail.com>
2024-06-04 10:52:52 +0530

    add models

---
+++ backend/models/userSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: "Users",
+  }
+);
+
+module.exports = mongoose.model("Users", userSchema);
